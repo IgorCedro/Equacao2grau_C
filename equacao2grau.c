0c59a395d47b9b5cbb2351e2b1f746232801a8db
IgorCedro <86737912+IgorCedro@users.noreply.github.com>
2023-08-21 19:19:16 -0300

    Update equacao2grau.c
    Foi alterado um dos comentários

--- equacao2grau.c
+++ equacao2grau.c
@@ -4,11 +4,11 @@
 void main()
 {
 /*
-2.  Dados A, B e C coeficientes de uma equaÁ„o de 2 grau, imprima suas raÌzes.
+    Dados A, B e C coeficientes de uma equa√ß√£o de 2 grau, imprima suas ra√≠zes.
 */
     setlocale(LC_ALL, "Portuguese");
 
-    //DeclaraÁ„o de vari·veis
+    //Declara√ß√£o de vari√°veis
     int continuar, num[3], delta;
     float raiz[2];
 
@@ -18,48 +18,48 @@ void main()
         //Entrada de dados
         do
         {
-            printf("Digite o coeficiente A da equaÁ„o de segundo grau: \n");
+            printf("Digite o coeficiente A da equa√ß√£o de segundo grau: \n");
             scanf("%d",&num[0]);
 
             if (num[0] == 0)
             {
-                printf("Tem que ser uma equaÁ„o de segundo grau, ou seja, A n„o pode ser 0! \n");
+                printf("Tem que ser uma equa√ß√£o de segundo grau, ou seja, A n√£o pode ser 0! \n");
                 printf("\n");//Pula a linha
             }
         }
         while(num[0] == 0);
 
-        printf("Digite o coeficiente B da equaÁ„o de segundo grau: \n");
+        printf("Digite o coeficiente B da equa√ß√£o de segundo grau: \n");
         scanf("%d",&num[1]);
 
-        printf("Digite o coeficiente C da equaÁ„o de segundo grau: \n");
+        printf("Digite o coeficiente C da equa√ß√£o de segundo grau: \n");
         scanf("%d",&num[2]);
 
         printf("\n");//Pula a linha
 
 
-        //Exibe a equaÁ„o com base nos coeficientes digitados.
-        printf("Sua equaÁ„o de segundo grau È: (%dx^2) + (%dx) + (%d) \n", num[0], num[1], num[2]);//ax^2 + bx + c
+        //Exibe a equa√ß√£o com base nos coeficientes digitados.
+        printf("Sua equa√ß√£o de segundo grau √©: (%dx^2) + (%dx) + (%d) \n", num[0], num[1], num[2]);//ax^2 + bx + c
 
         printf("\n");//Pula a linha
 
 
-        //C·lculo do Delta
+        //C√°lculo do Delta
         delta = pow(num[1], 2) - 4 * num[0] * num[2];//delta = (b ^ 2) - 4 * a * c
 
 
-        //Verifica o valor de delta para poder calcular a raÌz
-        if (delta < 0)//Menor que zero, n„o h· raiz
+        //Verifica o valor de delta para poder calcular a ra√≠z
+        if (delta < 0)//Menor que zero, n√£o h√° raiz
         {
-            printf("N„o h· raÌzes reais! \n");
+            printf("N√£o h√° ra√≠zes reais! \n");
             printf("\n");//Pula a linha
         }
-        else if (delta == 0)//Quando zero, raiz ˙nica
+        else if (delta == 0)//Quando zero, raiz √∫nica
         {
             raiz[0] = - num[1] / (2 * num[0]);//x = -b / 2 * a
 
-            //ExibiÁ„o
-            printf("A raÌz da equaÁ„o È: %.2f \n", raiz[0]);
+            //Exibi√ß√£o
+            printf("A ra√≠z da equa√ß√£o √©: %.2f \n", raiz[0]);
             printf("\n");//Pula a linha
         }
         else
@@ -67,20 +67,20 @@ void main()
             raiz[0] = (- num[1] + sqrt(delta))/(2 * num[0]);//x1 = (-b + sqrt(delta)) / 2 * a
             raiz[1] = (- num[1] - sqrt(delta))/(2 * num[0]);//x2 = (-b - sqrt(delta)) / 2 * a
 
-            //ExibiÁ„o
-            printf("As raÌzes da equaÁ„o s„o: %.2f e %.2f \n", raiz[0], raiz[1]);
+            //Exibi√ß√£o
+            printf("As ra√≠zes da equa√ß√£o s√£o: %.2f e %.2f \n", raiz[0], raiz[1]);
             printf("\n");//Pula a linha
         }
 
 
-        //Loop para validar a saÌda do programa
+        //Loop para validar a sa√≠da do programa
         do
             {
                 printf("Se deseja continuar, digite '1'. \n");
                 printf("Se deseja sair, digite '0'. \n");
                 scanf("%d",&continuar);
             }
-        while(continuar !=0 && continuar !=1);//Verifica se o n˙mero È diferente de 0 e 1
+        while(continuar !=0 && continuar !=1);//Verifica se o n√∫mero √© diferente de 0 e 1
 
 
 
